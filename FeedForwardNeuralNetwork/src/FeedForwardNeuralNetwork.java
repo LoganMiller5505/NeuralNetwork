c90aa53ef5dd1ee87edc252cafd8a47394fc3c0e
LoganMiller5505 <loganmiller5505@gmail.com>
2022-11-09 14:04:28 -0500

    Matrix-Oriented Network Beginning Worked on making the network more optimized through a matrix setup rather than through unique objects. This makes the code cleaner, easier to maintain, and overall better optimized for running. I also got the feedforward method to work on matrices, but still need to work on the backpropagation method next.

--- FeedForwardNeuralNetwork/src/FeedForwardNeuralNetwork.java
+++ FeedForwardNeuralNetwork/src/FeedForwardNeuralNetwork.java
@@ -1,159 +1,30 @@
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.List;
 
-public class FeedForwardNeuralNetwork {
-	
-	private int numWeights;
-	private int numBiases;
-	
-	private Random rand = new Random();
-	
-	private ArrayList<Layer> layers = new ArrayList<Layer>();
-	
-	private ArrayList<Connection> connections = new ArrayList<Connection>();
-	
-	
-	
-	public FeedForwardNeuralNetwork(int inputLayerSize, int hiddenLayerCount, int hiddenLayerSize, int outputLayerSize) {
-		
-		layers.add(new Layer(0, inputLayerSize));
-		
-		for(int i=0; i<hiddenLayerCount; i++) {
-			layers.add(new Layer(1, hiddenLayerSize));
-		}
-		
-		layers.add(new Layer(2, outputLayerSize));
-		
-		createConnections();
-		
-		populate();
-	}
-	
-	public void populate() {
-		for(int i = 0; i<getLayerCount(); i++) {
-			for(int k = 0; k<layers.get(i).getNeuronCount(); k++) {
-				if(rand.nextBoolean()) {
-					layers.get(i).getNeuron(k).setBias(rand.nextDouble());
-				}
-				else {
-					layers.get(i).getNeuron(k).setBias(-rand.nextDouble());
-				}
-				
-			}
-		}
-		
-		for(int i = 0; i<connections.size(); i++) {
-			if(rand.nextBoolean()) {
-				connections.get(i).setWeight(rand.nextDouble());
-			}
-			else {
-				connections.get(i).setWeight(-rand.nextDouble());
-			}
-		}
-		
-	}
-	
-	public void createConnections() {
-		for(int i = 0; i<getLayerCount()-1; i++) {
-			for(int k = 0; k<layers.get(i+1).getNeuronCount(); k++) {
-				for(int j=0; j<layers.get(i).getNeuronCount(); j++) {
-					connections.add(new Connection(i, k, j));
-				}
-			}
-		}
-	}
-	
-	
-	public void setInputActivations(double val) {
-		for(int i = 0; i<layers.get(0).getNeuronCount(); i++) {
-			layers.get(0).getNeuron(i).setActivation(val);;
-		}
-	}
-	
-	public void printOutputActivations() {
-		for(int i = 0; i<layers.get(layers.size()-1).getNeuronCount(); i++) {
-			System.out.println("Neuron " + i + " Output Activation: " + layers.get(layers.size()-1).getNeuron(i).getActivation());
-		}
-	}
-	
-	
-	public void feedforward() {
-		int currentWorkingIdx = 0;
-		for(int i = 1; i<getLayerCount(); i++) {
-			for(int k = 0; k<layers.get(i).getNeuronCount(); k++) {
-				double currentActivationVal = 0;
-				boolean validConnection = true;
-				int j = 0;
-				while(validConnection) {
-					currentActivationVal += sigmoid( ((layers.get(i-1).getNeuron(j).getActivation()) * (connections.get(currentWorkingIdx).getWeight())) + (layers.get(i).getNeuron(k).getBias()) );
-					j++;
-					currentWorkingIdx++;
-					if(connections.get(currentWorkingIdx).getFromIdx() >= j) {
-						validConnection=false;
-					}
-				}
-				layers.get(i).getNeuron(k).setActivation(sigmoid(currentActivationVal));
-			}
-		}
-	}
-	
-	
-	
-	
-	
-	public void printNeurons() {
-		for(int i = 0; i<getLayerCount(); i++) {
-			for(int k = 0; k<getLayer(i).getNeuronCount(); k++) {
-				System.out.println("Layer " + i + ", Neuron " + k + ": " + getLayer(i).getNeuron(k));
-			}
-		}
-	}
-	
-	public void printConnections() {
-		for(int i = 0; i<getConnectionCount(); i++) {
-			System.out.println(getConnection(i));
-		}
-	}
-	
-	
-	public void printParameters() {
-		System.out.println("Number of Weights: " + numWeights + ", Number of Biases: " + numBiases);
-	}
-	
-	
-	public double sigmoid(double x) {
-		return (1/(1+Math.exp(-x)));
-	}
-	
-	public double ReLU(double x) {
-		if(x<0) {
-			return 0;
-		}
-		else {
-			return x;
-		}
-	}
-	
-	
-	
-	
-	public Layer getLayer(int i) {
-		return layers.get(i);
-	}
-	
-	public int getLayerCount() {
-		return layers.size();
-	}
-	
-	
-	public Connection getConnection(int i) {
-		return connections.get(i);
-	}
-	
-	public int getConnectionCount() {
-		return connections.size();
-	}
-	
-	
-	
+public class FeedForwardNeuralNetwork{
+    Matrix weights_ih; //Weights for input -> hidden layer
+    Matrix weights_ho; //Weights for hidden -> output layer
+    Matrix bias_h; //Bias for hidden layer
+    Matrix bias_o; //Bias for output layer
+
+    public FeedForwardNeuralNetwork(int inputSize, int hiddenSize, int outputSize){
+        weights_ih = new Matrix(hiddenSize,inputSize);
+        weights_ho = new Matrix(outputSize,hiddenSize);
+
+        bias_h = new Matrix(hiddenSize, 1);
+        bias_o = new Matrix(outputSize, 1);
+    }
+
+    public List<Double> feedforward(double[] x){
+        Matrix input = Matrix.fromArray(x);
+        Matrix hidden = Matrix.multiply(weights_ih, input);
+        hidden.add(bias_h);
+        hidden.sigmoid();
+
+        Matrix output = Matrix.multiply(weights_ho, hidden);
+        output.add(bias_o);
+        output.sigmoid();
+
+        return output.toArray();
+    }
+
 }
--- FeedForwardNeuralNetwork/src/Runner.java
+++ FeedForwardNeuralNetwork/src/Runner.java
@@ -1,25 +1,10 @@
-
 public class Runner {
 
 	public static void main(String[] args) {
 		
-		FeedForwardNeuralNetwork nn = new FeedForwardNeuralNetwork(784, 2, 16, 10);
+		FeedForwardNeuralNetwork nn = new FeedForwardNeuralNetwork(3, 16, 10);
 		
-		//nn.createConnections();
-		//nn.populate();
-		
-		nn.printNeurons();
-		nn.printConnections();
-		nn.printParameters();
-		
-		System.out.println("Bias Count: " + Neuron.biasCount + ", Weight Count: " + Connection.weightCount);
-		
-		nn.setInputActivations(10000);
-		
-		nn.feedforward();
-		
-		nn.printOutputActivations();
-		
+		System.out.println(nn.feedforward(new double[] {1,1,0}));
 		
 	}
 
